65fd59e6defc57ee345da2912ab585ba336e30dc
Ranjit Kumar <95127402+ravikpt@users.noreply.github.com>
2022-12-26 17:34:06 +0400

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-
-# get power of a to b
 pow() {
 echo $(($1**$2))
+}
+shortest() {
+shortestArg=$(echo "${@}" | awk '{split($0,str," "); for (i in str) print length(str[i])}' | sort -n | head -n1)
+for i in "$@"
+ do
+  if [[ "${#i}" -eq "$shortestArg" ]]
+  then
+   echo "$i" 
+  fi
+ done
+}
+print_log() {
+echo "$(date +[\%Y-\%m-\%d" "\%R]) $@"
 }
